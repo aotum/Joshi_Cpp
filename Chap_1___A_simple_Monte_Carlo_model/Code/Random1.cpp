f9877fe4280446302a7e91678abef6a7ccbe19a3
aotum <1712118550@qq.com>
2023-06-17 09:14:00 +0800

    Update Random1.cpp
    add mathematical explanation

--- Chap_1___A_simple_Monte_Carlo_model/Code/Random1.cpp
+++ Chap_1___A_simple_Monte_Carlo_model/Code/Random1.cpp
@@ -16,7 +16,7 @@ double GetOneGaussianBySummation()
 	result -= 6.0;
 
 	return result;
-}
+}  // Central Limit Theorem
 
 double GetOneGaussianByBoxMuller()
 {
@@ -30,7 +30,10 @@ double GetOneGaussianByBoxMuller()
 		y = 2.0 * std::rand() / static_cast<double>(RAND_MAX) - 1;
 		sizeSquared = x * x + y * y;
 	} while (sizeSquared >= 1.0);
-	result = x * std::sqrt(-2 * std::log(sizeSquared) / sizeSquared);
+	result = x * std::sqrt(-2 * std::log(sizeSquared) / sizeSquared);  //  x * std::sqrt(sizeSquared) = cos(theta)
 
 	return result;
-}
+} // Box-Muller Transformation
+// sizeSquared= x * x + y * y ; theta = arctan(y / x), then sizeSquared is independent with theta and sizeSquared is distribited uniformly
+// between 0 and 1, and theta is distribution uniformly between 0 and 2*pi. From Box-Muller Transfromation, we have sqrt(-2 * 
+// log(sizeSquared)) * cos(theta) is distributed normally. Where cos(theta) = x / sqrt(sizeSquared).
